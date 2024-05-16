b3c6318ec55bf31f921a520a208eee0d193865c3
Dmitry <iamdimonis@gmail.com>
2024-05-16 17:25:23 +0300

    Connect ingredient edition with backend

--- app/src/main/java/ru/vsu/csf/bakebudget/api/RetrofitApi.kt
+++ app/src/main/java/ru/vsu/csf/bakebudget/api/RetrofitApi.kt
@@ -6,6 +6,8 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import ru.vsu.csf.bakebudget.models.request.IngredientRequestModel
 import ru.vsu.csf.bakebudget.models.request.UserSignUpRequestModel
 import ru.vsu.csf.bakebudget.models.response.UserSignInResponseModel
@@ -13,19 +15,19 @@ import ru.vsu.csf.bakebudget.models.request.UserSignInRequestModel
 import ru.vsu.csf.bakebudget.models.response.IngredientResponseModel
 
 interface RetrofitAPI {
-//    @FormUrlEncoded
     @POST("auth/signup")
     fun register(@Body userSignUpRequestModel: UserSignUpRequestModel?): Call<String?>?
 
     @POST("auth/signin")
     fun login(@Body userModel: UserSignInRequestModel?): Call<UserSignInResponseModel?>?
 
-//    @GET("ingredients/findAll")
-//    fun findAllIngredients(@Header("Authorization") authorization: String): Call<List<IngredientResponseModel>?>?
-
+//___________________________________________________________________________________________________________________________________
     @GET("ingredients/findAll")
     suspend fun findAllIngredients(@Header("Authorization") authorization: String): Response<List<IngredientResponseModel>?>?
 
     @POST("ingredients/create")
     suspend fun createIngredient(@Body ingredientModel: IngredientRequestModel?, @Header("Authorization") authorization: String): Response<IngredientResponseModel?>?
+
+    @PUT("ingredients/update/{id}")
+    suspend fun updateIngredient(@Path("id") id: Int, @Body ingredientModel: IngredientRequestModel?, @Header("Authorization") authorization: String): Response<IngredientResponseModel?>?
 }
--- app/src/main/java/ru/vsu/csf/bakebudget/components/Ingredient.kt
+++ app/src/main/java/ru/vsu/csf/bakebudget/components/Ingredient.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,20 +30,37 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import io.appmetrica.analytics.AppMetrica
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import ru.vsu.csf.bakebudget.DataIncorrectToast
 import ru.vsu.csf.bakebudget.R
+import ru.vsu.csf.bakebudget.api.RetrofitAPI
 import ru.vsu.csf.bakebudget.models.IngredientModel
+import ru.vsu.csf.bakebudget.models.request.IngredientRequestModel
+import ru.vsu.csf.bakebudget.models.response.IngredientResponseModel
 import ru.vsu.csf.bakebudget.ui.theme.SideBack
 
 @Composable
-fun Ingredient(ingredient: IngredientModel, color: Color, ingredients: MutableList<IngredientModel>, ingredientsSet : MutableSet<IngredientModel>) {
+fun Ingredient(
+    ingredient: IngredientResponseModel,
+    color: Color,
+    ingredients: MutableList<IngredientModel>,
+    ingredientsSet: MutableSet<IngredientModel>,
+    retrofitAPI: RetrofitAPI,
+    jwtToken: MutableState<String>,
+    ingredientsResponse: MutableList<IngredientResponseModel>
+) {
     val mContext = LocalContext.current
     val openAlertDialog = remember { mutableStateOf(false) }
     when {
         openAlertDialog.value -> {
             AlertDialog(
                 onDismissRequest = {
-                    openAlertDialog.value = false },
+                    openAlertDialog.value = false
+                },
                 onConfirmation = {
                     openAlertDialog.value = false
                 },
@@ -51,7 +69,10 @@ fun Ingredient(ingredient: IngredientModel, color: Color, ingredients: MutableLi
                 ingredient,
                 ingredients,
                 mContext,
-                ingredientsSet
+                ingredientsSet,
+                retrofitAPI = retrofitAPI,
+                jwtToken = jwtToken,
+                ingredientsResponse
             )
         }
     }
@@ -59,28 +80,37 @@ fun Ingredient(ingredient: IngredientModel, color: Color, ingredients: MutableLi
         modifier = Modifier
             .fillMaxWidth()
             .background(color)
-            .padding(5.dp)) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .defaultMinSize(minHeight = 50.dp)
-            .background(color)
-            .padding(start = 21.dp, end = 5.dp),
-            verticalAlignment = Alignment.CenterVertically) {
-            Box(modifier = Modifier.fillMaxWidth(0.33f),
-                contentAlignment = Alignment.CenterStart) {
+            .padding(5.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 50.dp)
+                .background(color)
+                .padding(start = 21.dp, end = 5.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(0.33f),
+                contentAlignment = Alignment.CenterStart
+            ) {
                 Text(text = ingredient.name, maxLines = 3)
             }
-            Box(modifier = Modifier.fillMaxWidth(0.5f),
-                contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier.fillMaxWidth(0.5f),
+                contentAlignment = Alignment.Center
+            ) {
                 Text(text = ingredient.weight.toString(), maxLines = 3)
             }
-            Box(modifier = Modifier.fillMaxWidth(0.75f),
-                contentAlignment = Alignment.CenterEnd) {
+            Box(
+                modifier = Modifier.fillMaxWidth(0.75f),
+                contentAlignment = Alignment.CenterEnd
+            ) {
                 Text(text = ingredient.cost.toString(), maxLines = 3)
             }
             TextButton(
                 onClick = { openAlertDialog.value = true }
-            ){
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.options),
                     contentDescription = "options"
@@ -96,10 +126,13 @@ fun AlertDialog(
     onConfirmation: () -> Unit,
     dialogTitle: String,
     dialogText: String,
-    ingredient: IngredientModel,
+    ingredient: IngredientResponseModel,
     ingredients: MutableList<IngredientModel>,
     context: Context,
-    ingredientsSet : MutableSet<IngredientModel>
+    ingredientsSet: MutableSet<IngredientModel>,
+    retrofitAPI: RetrofitAPI,
+    jwtToken: MutableState<String>,
+    ingredientsResponse: MutableList<IngredientResponseModel>,
 ) {
     val name = remember {
         mutableStateOf(ingredient.name)
@@ -133,27 +166,49 @@ fun AlertDialog(
         confirmButton = {
             TextButton(
                 onClick = {
-                    if (name.value.isEmpty() || weight.value.isEmpty() || weight.value.toIntOrNull() == null || cost.value.isEmpty() || cost.value.toIntOrNull() == null || ingredientsSet.contains(IngredientModel(
-                            name.value,
-                            weight.value.toInt(),
-                            cost.value.toInt()
-                        ))) {
+                    if (name.value.isEmpty() || weight.value.isEmpty() || weight.value.toIntOrNull() == null || cost.value.isEmpty() || cost.value.toIntOrNull() == null || ingredientsSet.contains(
+                            IngredientModel(
+                                name.value,
+                                weight.value.toInt(),
+                                cost.value.toInt()
+                            )
+                        )
+                    ) {
                         DataIncorrectToast(context)
                     } else {
+                        update(
+                            context, retrofitAPI, jwtToken, ingredient, IngredientRequestModel(
+                                name.value,
+                                weight.value.toInt(),
+                                cost.value.toInt()
+                            )
+                        )
                         val eventParameters1 = "{\"button_clicked\":\"ingredient_edit\"}"
                         AppMetrica.reportEvent("Ingredient edited", eventParameters1)
-                        ingredients.remove(ingredient)
-                        ingredientsSet.remove(ingredient)
-                        ingredients.add(IngredientModel(
+                        ingredients.remove(IngredientModel(ingredient.name, ingredient.weight, ingredient.cost))
+                        ingredientsResponse.remove(ingredient)
+                        ingredientsSet.remove(IngredientModel(ingredient.name, ingredient.weight, ingredient.cost))
+                        ingredients.add(
+                            IngredientModel(
+                                name.value,
+                                weight.value.toInt(),
+                                cost.value.toInt()
+                            )
+                        )
+                        ingredientsResponse.add(IngredientResponseModel(
+                            id = ingredient.id,
                             name.value,
                             weight.value.toInt(),
                             cost.value.toInt()
-                        ))
-                        ingredientsSet.add(IngredientModel(
-                            name.value,
-                            weight.value.toInt(),
-                            cost.value.toInt()
-                        ))
+                        )
+                        )
+                        ingredientsSet.add(
+                            IngredientModel(
+                                name.value,
+                                weight.value.toInt(),
+                                cost.value.toInt()
+                            )
+                        )
                         onConfirmation()
                     }
                 }
@@ -166,8 +221,9 @@ fun AlertDialog(
                 onClick = {
                     val eventParameters1 = "{\"button_clicked\":\"ingredient_delete\"}"
                     AppMetrica.reportEvent("Ingredient deleted", eventParameters1)
-                    ingredients.remove(ingredient)
-                    ingredientsSet.remove(ingredient)
+                    ingredients.remove(IngredientModel(ingredient.name, ingredient.weight, ingredient.cost))
+                    ingredientsSet.remove(IngredientModel(ingredient.name, ingredient.weight, ingredient.cost))
+                    ingredientsResponse.remove(ingredient)
                     onDismissRequest()
                 }
             ) {
@@ -175,4 +231,30 @@ fun AlertDialog(
             }
         }
     )
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+private fun update(
+    ctx: Context,
+    retrofitAPI: RetrofitAPI,
+    jwtToken: MutableState<String>,
+    ingredient: IngredientResponseModel,
+    ingredientRequestModel: IngredientRequestModel
+) {
+    GlobalScope.launch(Dispatchers.Main) {
+        val res =
+            retrofitAPI.updateIngredient(ingredient.id, ingredientRequestModel, "Bearer ".plus(jwtToken.value))
+        onResultUpdate(res, ctx)
+    }
+}
+
+fun onResultUpdate(
+    result: Response<IngredientResponseModel?>?,
+    ctx: Context
+) {
+    Toast.makeText(
+        ctx,
+        "Response Code : " + result!!.code() + "\n" + result.body(),
+        Toast.LENGTH_SHORT
+    ).show()
 }
